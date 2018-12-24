df620e4db17286645da336343868abc98dda4635
Elia <eliaromano2000@gmail.com>
2018-12-24 18:06:08 +0100

    commit

--- php/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-    define('DB_HOST','');
-    define('DB_USER','');
-    define('DB_PASSWORD','');
-    define('DB_NAME','');
-
-    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die("Si è verificato un errore durate la connesione al database")
-
-?>
